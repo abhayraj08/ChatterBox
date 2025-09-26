220883459400b5b58b45831f441bec6cae6f1363
abhayraj08 <mathematics7878@gmail.com>
2025-09-26 13:05:16 +0530

    Ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,17 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
+const path = require('path');
+
+// Serve frontend build
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log("MongoDB connected"))
     .catch((err) => console.error(err));
 
@@ -20,5 +24,11 @@ mongoose.connect(process.env.MONGO_URI)
 const authRoutes = require("./routes/auth");
 app.use("/api/auth", authRoutes);
 
+// catch-all route
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+// Starting server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
